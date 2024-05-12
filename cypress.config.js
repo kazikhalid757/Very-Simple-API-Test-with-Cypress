7bfcdbb2084402cc345a3e98a36a379c8d901c49
Kazi Khalid Hasan Tamim <113050909+kazikhalid757@users.noreply.github.com>
2024-05-12 21:01:46 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,44 +1,18 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // Implement a node event listener
-//       on("before:browser:launch", (browser, launchOptions) => {
-//         // Customize browser launch options
-//         if (browser.name === "chrome") {
-//           launchOptions.args.push("--disable-dev-shm-usage");
-//         }
-//       });
-//     },
-//   },
-// });
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "cypress-allure-plugin",
-  env: {
-    // Add any environment variables if needed
-  },
-  pluginsFile: false, // Disable default plugins file to avoid conflicts
   e2e: {
     setupNodeEvents(on, config) {
-      // Implement a node event listener for end-to-end tests
+      // Implement a node event listener
       on("before:browser:launch", (browser, launchOptions) => {
-        // Customize browser launch options for end-to-end tests
+        // Customize browser launch options
         if (browser.name === "chrome") {
           launchOptions.args.push("--disable-dev-shm-usage");
         }
       });
     },
   },
-  component: {
-    setupNodeEvents(on, config) {
-      // Implement a node event listener for component tests (if applicable)
-      // This can be left empty if you're not using component tests
-    },
-  },
 });
+
 
 
