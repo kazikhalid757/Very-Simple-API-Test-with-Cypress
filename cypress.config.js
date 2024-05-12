2937b3898c8e9a6a182d90b042a4ce722000d771
Kazi Khalid Hasan Tamim <113050909+kazikhalid757@users.noreply.github.com>
2024-05-12 20:58:05 +0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,27 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // Implement a node event listener
+//       on("before:browser:launch", (browser, launchOptions) => {
+//         // Customize browser launch options
+//         if (browser.name === "chrome") {
+//           launchOptions.args.push("--disable-dev-shm-usage");
+//         }
+//       });
+//     },
+//   },
+// });
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-allure-plugin",
+  env: {
+    // Add any environment variables if needed
+  },
+  pluginsFile: false, // Disable default plugins file to avoid conflicts
   e2e: {
     setupNodeEvents(on, config) {
       // Implement a node event listener
@@ -13,6 +34,5 @@ module.exports = defineConfig({
     },
   },
 });
-
 
 
